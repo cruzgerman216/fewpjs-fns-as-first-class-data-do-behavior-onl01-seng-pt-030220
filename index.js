3ff0b1530c554fd8753a808b1dc12cc96272632b
German Cruz <cruzgerman216@yahoo.com>
2020-09-16 18:30:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,5 +10,13 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 function greet(str){
-  console.log(str);
+  let time = praseInt(str.split(":")[0]);
+  if time == 12 {
+    return "Good Morning";
+  }else if time > 12 && time < 5{
+    return "Good Afternoon"
+  }else {
+    return "Good Evening";
+  }
+  
 }
